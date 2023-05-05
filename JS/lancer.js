1cc35f7d064e8e9362c283a9f78bbb337e689962
GentleMizt <olamideiyanda18@gmail.com>
2023-05-05 23:31:14 +0100

    progress

--- JS/lancer.js
+++ JS/lancer.js
@@ -2,7 +2,17 @@
 const sectionSports = document.querySelector(".section__sport");
 const sectionNews = document.querySelector(".section__news");
 const sectionEvents = document.querySelector(".section__events");
+const listSections = document.querySelectorAll('.headlines__text');
 
 const sport = document.getElementById("sports");
 const news = document.getElementById("news");
 const events = document.getElementById("events");
+
+// function for switching through the sections
+
+for (let i = 0; i < listSections.length; i++) {
+ const element = listSections[i].classList.contains('headline__active');
+ console.log(element);
+}
+const switchSection = () =>{
+}
