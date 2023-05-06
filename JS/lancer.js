b5cfd5f38f42befbd5fd6d371484d5e68d66dfac
GentleMizt <olamideiyanda18@gmail.com>
2023-05-06 16:16:42 +0100

    progress

--- JS/lancer.js
+++ JS/lancer.js
@@ -1,7 +1,7 @@
 "use strict";
 const sectionSports = document.querySelector(".section__sport");
-const sectionNews = document.querySelector(".section__news");
-const sectionEvents = document.querySelector(".section__events");
+// const sectionNews = document.querySelector(".section__news");
+// const sectionEvents = document.querySelector(".section__events");
 
 const listSections = document.querySelectorAll(".headlines__text");
 // console.log(listSections);
@@ -13,11 +13,18 @@ const events = document.getElementById("events");
 // function for switching through the sections
 
 listSections.forEach((list) => {
-  list.addEventListener("click", () => {
+  list.addEventListener("click", (e) => {
     listSections.forEach((list) => {
       list.classList.remove("headline__active");
     });
-    list.classList.add("headline__active");
+    e.target.classList.add("headline__active");
+    if (e.target == news) {
+      sectionSports.style.backgroundImage = "url(/img/blue-bg.jpeg)";
+    } else if (e.target == events){
+        sectionSports.style.backgroundImage = "url(/img/pink-bg.jpeg)";       
+    }  else if (e.target == sport){
+        sectionSports.style.backgroundImage = "url(/img/green-bg.jpeg)";       
+    } 
 });
 });
 
