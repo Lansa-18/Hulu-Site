b6664dc3175f2fbcf9ce3f858bac993d42585825
GentleMizt <olamideiyanda18@gmail.com>
2023-05-06 15:00:27 +0100

    progress

--- JS/lancer.js
+++ JS/lancer.js
@@ -3,7 +3,7 @@ const sectionSports = document.querySelector(".section__sport");
 const sectionNews = document.querySelector(".section__news");
 const sectionEvents = document.querySelector(".section__events");
 
-const listSections = document.querySelectorAll('.headlines__text');
+const listSections = document.querySelectorAll(".headlines__text");
 // console.log(listSections);
 
 const sport = document.getElementById("sports");
@@ -12,15 +12,14 @@ const events = document.getElementById("events");
 
 // function for switching through the sections
 
-listSections.forEach(list =>{
-    list.addEventListener('click', changeSection);
+listSections.forEach((list) => {
+  list.addEventListener("click", () => {
+    listSections.forEach((list) => {
+      list.classList.remove("headline__active");
+    });
+    list.classList.add("headline__active");
 });
+});
+
 // const switchSection = () =>{
 // }
-
-const changeSection = () =>{
-    listSections.forEach(list =>{
-        list.classList.remove('headline__active');
-    });
-    list.classList.add('headline__acitve');
-}
